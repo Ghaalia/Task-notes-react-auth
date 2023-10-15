887f90c915dc2f3340ed3a07355cb064a0f2bb9a
Ghaalia <Ghaliaals9@gmail.com>
2023-10-15 20:22:53 +0300

    DONE

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,9 @@
 import instance from ".";
+import jwt_decode from "jwt-decode";
 
 const login = async (userInfo) => {
   const { data } = await instance.post("/auth/login", userInfo);
+  storeToken(data?.token);
   return data;
 };
 
@@ -10,7 +12,32 @@ const register = async (userInfo) => {
   for (let key in userInfo) formData.append(key, userInfo[key]);
 
   const { data } = await instance.post("/auth/register", formData);
+  storeToken(data?.token);
   return data;
+};
+
+const storeToken = (token) => {
+  localStorage.setItem("token", token);
+};
+
+const checkToken = () => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    const decode = jwt_decode(token);
+    const TimeNow = Date.now() / 1000;
+    console.log(decode.exp, TimeNow);
+    console.log(decode.exp > TimeNow);
+    if (decode.exp < TimeNow) {
+      localStorage.removeItem("token");
+      return false;
+    }
+    return true;
+  }
+  return false;
+};
+
+const logout = () => {
+  localStorage.removeItem("token");
 };
 
 const me = async () => {
@@ -23,4 +50,4 @@ const getAllUsers = async () => {
   return data;
 };
 
-export { login, register, me, getAllUsers };
+export { login, register, me, getAllUsers, storeToken, checkToken, logout };
